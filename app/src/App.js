d9e22ad5c171a368d350c70ac2fd6d81ba9ea0fd
folonidaniel <folonidaniel@yahoo.com>
2025-05-24 23:30:41 -0300

    feat..add-missing-pages

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,14 @@ import { AboutUs } from "./pages/AboutUs"
 import { Search } from "./pages/Search"
 import { MyAccount } from './pages/MyAccout';
 import { Checkout } from './pages/Checkout';
-import { Main } from "./pages/Main"
+import { Main } from "./pages/Main";
+import {AdminCreateUser} from "./pages/AdminCreateUser";
+import {AdminCreateProduct} from "./pages/AdminCreateProduct";
+import {AdminEditProduct} from "./pages/AdminEditProduct";
+import {AdminEditUser} from "./pages/AdminEditUser";
+import {AdminMain} from "./pages/AdminMain";
+import {AdminSearchProduct} from './pages/AdminSearchProduct';
+import {AdminSearchUsers} from './pages/AdminSearchUsers';
 
 function App() {
   return (
@@ -23,6 +30,13 @@ function App() {
         <Route path="/search" element={<Search/>}/>
         <Route path="/my-account" element={<MyAccount/>}/>
         <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/create-user" element={<AdminCreateUser/>}/>
+        <Route path="/create-product" element={<AdminCreateProduct/>}/>
+        <Route path="/edit-product" element={<AdminEditProduct/>}/>
+        <Route path="/edit-user" element={<AdminEditUser/>}/>
+        <Route path="/main-admin" element={<AdminMain/>}/>
+        <Route path="/search-product-admin" element={<AdminSearchProduct/>}/>
+        <Route path="/search-users-admin" element={<AdminSearchUsers/>}/>
       </Routes>
     </BrowserRouter>
   );
